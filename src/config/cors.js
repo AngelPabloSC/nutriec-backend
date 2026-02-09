f0e99d64d00f7446ab6486e9d9ac89cfc6fbac71
AngelPabloSC <138425945+AngelPabloSC@users.noreply.github.com>
2026-02-09 02:50:21 -0500

    Update cors.js

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,21 +1,8 @@
 const corsOptions = {
-    origin: function (origin, callback) {
-        const allowedOrigins = process.env.ALLOWED_ORIGINS
-            ? process.env.ALLOWED_ORIGINS.split(',')
-            : ['http://localhost:5173', 'http://localhost:3000'];
-
-        // Allow requests with no origin (like mobile apps, Postman, curl)
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
+    // Permitir conexiones desde cualquier origen
+    origin: '*',
+    // Nota: credentials debe ser false cuando origin es '*'
+    credentials: false,
     optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
